1c007addcfb5266457345277c0359be147968379
niloytalukde <niloytalukderme@gmail.com>
2025-07-12 00:46:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InvoiceGenerator from './Components/InvoiceGenerator';
+
+const App = () => {
+  return (
+    <div>
+     <InvoiceGenerator></InvoiceGenerator>
+    </div>
+  );
+};
+
+export default App;
